3ec2259588b4adf57af7b12cd65ff9add9f80e74
maia rojas <maia.rt.46@gmail.com>
2017-10-01 16:19:50 -0500

    Estructura base

---
+++ src/assets/js/main.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const root = $(".root");
+const render = (root) => {
+    root.empty();
+    const wrapper = $('<div class="wrapper"></div>');
+
+
+    root.append(wrapper);
+
+
+}
+
+const updated = function () {
+    render(root);
+}
+
+const state = {
+  pagina: null,
+  dia: new Date()
+}
+
+
+$(_ => {
+  var config = {
+    apiKey: "AIzaSyBdR77WTmQPJ4ByaPCsNRxJiKSxpPxYmfU",
+    authDomain: "samugo-ffbbd.firebaseapp.com",
+    databaseURL: "https://samugo-ffbbd.firebaseio.com",
+    projectId: "samugo-ffbbd",
+    storageBucket: "samugo-ffbbd.appspot.com",
+    messagingSenderId: "955932477926"
+  };
+
+   firebase.initializeApp(config);
+
+   var database = firebase.database();
+
+    database.ref().on("value", function(snap){
+
+    state.total = snap.val();
+    console.log(state.total);
+
+    const root = $(".root");
+    render(root);
+
+  });
+});
